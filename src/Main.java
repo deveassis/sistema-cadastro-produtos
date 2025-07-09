f51c69465de702e404d7fd64b591d4002f420610
{DevAssis} Gabriel Assis <154300021+deveassis@users.noreply.github.com>
2025-07-08 23:47:27 -0300

    switch case na main para o usuario escolher as opcoes de atendimento

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Model.Produto;
 import Repository.ProdutoRepository;
 import Service.ProdutoService;
 
+import java.lang.classfile.instruction.SwitchCase;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -10,26 +11,46 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       ProdutoService service = new ProdutoService();
-      // Cadastrar Produtos
-        System.out.println("--------------------------");
-        System.out.println("Cadastrando produtos");
-
-      Scanner myProduct = new Scanner(System.in);
-      myProduct.useLocale(Locale.US); // <- aceita ponto decimal como 12.30
 
 
-      ArrayList<String> listaOpcoes = new ArrayList<String>();
+      System.out.println("--------------------------");
+      System.out.println("Escolha uma das opções abaixo: ");
+      Scanner optionsTerm = new Scanner(System.in);
+        System.out.println("1 - Cadastrar Produto ");
+        System.out.println("2 - Listar Produtos ");
+        System.out.println("3 - Atualizar  Produto");
+        System.out.println("4 - Pesquisar produto por nome");
+        int optionInsert = optionsTerm.nextInt();
+        switch(optionInsert){
+          case  1:
+              System.out.println("Vamos cadastrar um novo produto!");
+                Scanner myProduct = new Scanner(System.in);
+                 myProduct.useLocale(Locale.US); // <- aceita ponto decimal como 12.30
+              System.out.println("Informe o nome do produto: ");
+                 String name = myProduct.nextLine();
+              System.out.println("Informe o preco do produto: ");
+                  double preco = myProduct.nextDouble();
+              System.out.println("Informe a quantidade de itens: ");
+                 int quantidade = myProduct.nextInt();
+                 service.createProduto(name, preco, quantidade);
+              break;
+          case 2:
+              System.out.println("Estamos carregando a lista de produtos...");
+              service.listarProdutos();
 
-      System.out.println("Nome: ");
-      String name = myProduct.nextLine();
+            case 3:
+                System.out.println("Informe abaixo o id do produto que voce deseja atualizar o preco: ");
+                Scanner updateProduct = new Scanner(System.in);
+                int  searchId = updateProduct.nextInt();
+                System.out.println("Ok, agora informe o novo nome do produto: ");
+                String nameProduct = updateProduct.nextLine();
+                System.out.println("Ok, agora informe o novo preco: ");
+                Double priceProduct = updateProduct.nextDouble();
+                System.out.println("Ok, agora informe a quantidade: ");
+                int quantityProduct = updateProduct.nextInt();
 
-      System.out.println("Preço: ");
-      double preco = myProduct.nextDouble();
 
-      System.out.println("Quantidade: ");
-      int quantidade = myProduct.nextInt();
-
-      service.createProduto(name, preco, quantidade);
+        }
 
 
     }
